38dbb8395b01160a547729fb68d9f2600ff7d095
Jose Angel <ue137669@edu.xunta.gal>
2022-03-06 18:50:43 +0100

    Ocurrencias  Circulo  por Aro

--- src/test/java/Aro/AroTest.java
+++ src/test/java/Aro/AroTest.java
@@ -1,8 +1,8 @@
 /**
- * Probas para a clase Circulo
+ * Probas para a clase Aro
  * @author Jose Angel Santoveña Gomez
  */
-package circulo;
+package Aro;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
